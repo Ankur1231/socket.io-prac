361f4aa8a90e15fb8a2fe82340d8b8a8c623f971
Ankur Udey <ankurudey@gmail.com>
2021-11-19 22:37:54 +0530

    second commit

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,16 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Chat = ({ socket, userName, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
-  const sendMessage = () => {};
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room,
+        author: userName,
+        message: currentMessage,
+        time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+    }
+  };
+
+  useEffect(() => {
+    socket.on("recieve_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
 
   return (
-    <div>
+    <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+        {messageList.map((data) => (
+          <p>{data.message}</p>
+        ))}
+      </div>
       <div className="chat-footer">
         <input
           type="text"
